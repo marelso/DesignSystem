1d10a08838d5d38eb2acd384ec88e3c8a8f19df1
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 20:14:33 -0300

    Including new file to handle home list title

--- app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
+++ app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import com.marelso.designsystem.ui.home.components.HomeBodyComponent
 import com.marelso.designsystem.ui.home.components.HomeHeadlineComponent
 import com.marelso.designsystem.ui.home.components.HomeSubtitleComponent
+import com.marelso.designsystem.ui.home.components.HomeTitleComponent
 import com.marelso.designsystem.ui.theme.dimen2
 import com.marelso.designsystem.ui.theme.dimen4
 
@@ -15,6 +16,7 @@ fun HomeScreenHoisting() {
     Column(modifier = Modifier.padding(dimen4)) {
         HomeHeadlineComponent(modifier = Modifier.padding(bottom = dimen2))
         HomeSubtitleComponent(modifier = Modifier.padding(bottom = dimen2))
-        HomeBodyComponent(modifier = Modifier.padding(bottom = dimen2))
+        HomeBodyComponent(modifier = Modifier.padding(bottom = dimen4))
+        HomeTitleComponent(modifier = Modifier.padding(bottom = dimen2))
     }
 }
---
+++ app/src/main/java/com/marelso/designsystem/ui/home/components/HomeTitleComponent.kt
@@ -0,0 +1,12 @@
+package com.marelso.designsystem.ui.home.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.marelso.designsystem.components.TitleComponent
+
+@Composable
+fun HomeTitleComponent(
+    modifier: Modifier = Modifier
+) {
+    TitleComponent(modifier = modifier, text = "Thats the current list of components")
+}

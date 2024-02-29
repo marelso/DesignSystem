a34f7cfd1840c5e173aa42cc6f623b9684f6b4ef
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 18:39:24 -0300

    Including new composable at screen hoisting

--- app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
+++ app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
@@ -1,6 +1,9 @@
 package com.marelso.designsystem.ui.home
 
 import androidx.compose.runtime.Composable
+import com.marelso.designsystem.ui.home.components.HomeHeadline
 
 @Composable
-fun HomeScreenHoisting() {}
+fun HomeScreenHoisting() {
+    HomeHeadline()
+}

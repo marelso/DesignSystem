fc409e31e55ae2eab42704ec12b3cdb526c58f65
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 18:45:50 -0300

    Including new home component

---
+++ app/src/main/java/com/marelso/designsystem/ui/home/components/HomeSubtitleComponent.kt
@@ -0,0 +1,9 @@
+package com.marelso.designsystem.ui.home.components
+
+import androidx.compose.runtime.Composable
+import com.marelso.designsystem.components.SubtitleComponent
+
+@Composable
+fun HomeSubtitleComponent() {
+    SubtitleComponent()
+}

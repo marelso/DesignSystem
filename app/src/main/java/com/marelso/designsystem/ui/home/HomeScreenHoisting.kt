b7de8b7647d52ffa874d421d3958e2cd5607a874
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 19:23:36 -0300

    Including new Component of captions inside home

--- app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
+++ app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.marelso.designsystem.ui.home.components.HomeCaptionComponent
 import com.marelso.designsystem.ui.home.components.HomeHeadlineComponent
 import com.marelso.designsystem.ui.home.components.HomeSubtitleComponent
 import com.marelso.designsystem.ui.theme.dimen4
@@ -13,5 +14,6 @@ fun HomeScreenHoisting() {
     Column(modifier = Modifier.padding(dimen4)) {
         HomeHeadlineComponent()
         HomeSubtitleComponent()
+        HomeCaptionComponent()
     }
 }

4c9f1e5ed43319a22e4e198b4f6b37d366893d97
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 18:42:15 -0300

    Including dimen file

---
+++ app/src/main/java/com/marelso/designsystem/ui/theme/dimen.kt
@@ -0,0 +1,15 @@
+package com.marelso.designsystem.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val dimen6 = 24.dp
+val dimen5 = 20.dp
+val dimen4 = 16.dp
+val dimen3 = 12.dp
+val dimen2 = 8.dp
+val dimen1 = 4.dp
+val dimen0 = 2.dp
+
+val buttonSmall = 40.dp
+val buttonMedium = 60.dp
+val buttonLarge = 80.dp

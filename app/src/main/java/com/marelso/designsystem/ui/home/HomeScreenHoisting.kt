208f4061525e2ea808936193e4827b9d5f32335d
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 19:29:50 -0300

    removing wrong import

--- app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
+++ app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.marelso.designsystem.ui.home.components.HomeBodyComponent
 import com.marelso.designsystem.ui.home.components.HomeCaptionComponent
 import com.marelso.designsystem.ui.home.components.HomeHeadlineComponent
 import com.marelso.designsystem.ui.home.components.HomeSubtitleComponent

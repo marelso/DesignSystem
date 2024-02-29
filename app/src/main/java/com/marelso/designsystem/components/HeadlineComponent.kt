1e19ae8fb9698ab0a66d1d45fd886cf76fb9374a
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 18:39:01 -0300

    Updating headline component title

--- app/src/main/java/com/marelso/designsystem/components/HeadlineComponent.kt
+++ app/src/main/java/com/marelso/designsystem/components/HeadlineComponent.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
-fun Headline(
+fun HeadlineComponent(
     modifier: Modifier = Modifier,
     text: String,
     textAlign: TextAlign = TextAlign.Start,

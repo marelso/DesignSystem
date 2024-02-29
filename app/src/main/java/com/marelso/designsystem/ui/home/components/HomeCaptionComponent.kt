7dc90c16307c4f3aae59cd0f8e140eaf5d3c941a
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 19:29:35 -0300

    Including caption at home screen

---
+++ app/src/main/java/com/marelso/designsystem/ui/home/components/HomeCaptionComponent.kt
@@ -0,0 +1,9 @@
+package com.marelso.designsystem.ui.home.components
+
+import androidx.compose.runtime.Composable
+import com.marelso.designsystem.components.CaptionComponent
+
+@Composable
+fun HomeCaptionComponent() {
+    CaptionComponent(text = "Here we have all the available components ready to test!")
+}

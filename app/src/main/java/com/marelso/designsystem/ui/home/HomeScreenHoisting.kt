75f3506538d9829ab933172526977dafc4858260
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 20:01:02 -0300

    Changing component

--- app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
+++ app/src/main/java/com/marelso/designsystem/ui/home/HomeScreenHoisting.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.marelso.designsystem.ui.home.components.HomeCaptionComponent
+import com.marelso.designsystem.ui.home.components.HomeBodyComponent
 import com.marelso.designsystem.ui.home.components.HomeHeadlineComponent
 import com.marelso.designsystem.ui.home.components.HomeSubtitleComponent
 import com.marelso.designsystem.ui.theme.dimen2
@@ -15,6 +15,6 @@ fun HomeScreenHoisting() {
     Column(modifier = Modifier.padding(dimen4)) {
         HomeHeadlineComponent(modifier = Modifier.padding(bottom = dimen2))
         HomeSubtitleComponent(modifier = Modifier.padding(bottom = dimen2))
-        HomeCaptionComponent(modifier = Modifier.padding(bottom = dimen2))
+        HomeBodyComponent(modifier = Modifier.padding(bottom = dimen2))
     }
 }

214d893594a8a04901435bcb31efe065915e9df6
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-02-29 18:39:49 -0300

    Including new component at home screen to handle headline text

---
+++ app/src/main/java/com/marelso/designsystem/ui/home/components/HomeHeadline.kt
@@ -0,0 +1,9 @@
+package com.marelso.designsystem.ui.home.components
+
+import androidx.compose.runtime.Composable
+import com.marelso.designsystem.components.HeadlineComponent
+
+@Composable
+fun HomeHeadline() {
+    HeadlineComponent(text = "Testing")
+}

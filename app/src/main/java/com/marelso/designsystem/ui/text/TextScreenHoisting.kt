4d8105db7a785f365e75c49eb5ee0bbb5af19917
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-03-01 01:42:51 -0300

    Applying paddings

--- app/src/main/java/com/marelso/designsystem/ui/text/TextScreenHoisting.kt
+++ app/src/main/java/com/marelso/designsystem/ui/text/TextScreenHoisting.kt
@@ -1,13 +1,17 @@
 package com.marelso.designsystem.ui.text
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.marelso.designsystem.components.typography.BodyComponent
 import com.marelso.designsystem.components.typography.CaptionComponent
 import com.marelso.designsystem.components.typography.HeadlineComponent
 import com.marelso.designsystem.components.typography.SubtitleComponent
 import com.marelso.designsystem.components.typography.TitleComponent
+import com.marelso.designsystem.ui.theme.dimen2
+import com.marelso.designsystem.ui.theme.dimen4
 
 @Composable
 fun TextScreenHoisting(navHostController: NavHostController) {
@@ -16,11 +20,11 @@ fun TextScreenHoisting(navHostController: NavHostController) {
 
 @Composable
 fun TextScreen() {
-    Column {
-        HeadlineComponent(text = "Header text")
-        SubtitleComponent(text = "Header text")
-        TitleComponent(text = "Header text")
-        CaptionComponent(text = "Header text")
-        BodyComponent(text = "Header text")
+    Column(modifier = Modifier.padding(dimen4)) {
+        HeadlineComponent(modifier = Modifier.padding(bottom = dimen2), text = "Header text")
+        SubtitleComponent(modifier = Modifier.padding(bottom = dimen2), text = "Header text")
+        TitleComponent(modifier = Modifier.padding(bottom = dimen2), text = "Header text")
+        BodyComponent(modifier = Modifier.padding(bottom = dimen2), text = "Header text")
+        CaptionComponent(modifier = Modifier.padding(bottom = dimen2), text = "Header text")
     }
 }

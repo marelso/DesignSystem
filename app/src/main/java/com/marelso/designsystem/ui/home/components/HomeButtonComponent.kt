a962ba4fa92cea5d072a2b3760e03011a0d31d94
Marcelo Klein <marcelo.klein@townsq.com.br>
2024-03-01 00:21:54 -0300

    Including component to handle button nav

---
+++ app/src/main/java/com/marelso/designsystem/ui/home/components/HomeButtonComponent.kt
@@ -0,0 +1,17 @@
+package com.marelso.designsystem.ui.home.components
+
+import android.app.Notification.Action
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.marelso.designsystem.R
+import com.marelso.designsystem.components.button.ActionButtonComponent
+import com.marelso.designsystem.components.button.data.ButtonAction
+
+@Composable
+fun HomeButtonComponent() {
+    ActionButtonComponent(action = ButtonAction(
+        label = "asdhuadsuhsaf",
+        onClick = {}
+    ))
+}
